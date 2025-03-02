d1c1f46bdb086fcd7fc5c9353a12bc3e1f6ea60d
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2025-03-02 17:53:22 +0100

    js test for form with textarea elements

---
+++ js/textForm.js
@@ -0,0 +1,138 @@
+
+let namesObj = {
+
+	render: function() {
+		let namGridSec = new azulInpGrid({rows: 3, cols: 1, name: 'names'});
+		let namgrid = namGridSec.inpgrid;
+
+		this.inp1 = new azulInp({Field: 'First', Length: '150px', idx: 1});
+		this.inp2 = new azulInp({Field: 'Middle', Length: '200px', idx: 2});
+		this.inp3 = new azulInp({Field: 'Last', Length: '200px', idx: 3});
+
+		let firstRow = namgrid.els[0][0];
+		firstRow.appendChild(this.inp1.getInpEl());
+		firstRow.appendChild(this.inp2.getInpEl());
+		firstRow.appendChild(this.inp3.getInpEl());
+
+		let secRow = namgrid.els[1][0];
+		this.inp4 = new azulInp({Field: 'Email',Length: '250px', idx: 4});
+		secRow.appendChild(this.inp4.getInpEl());
+
+
+		let thirdRow = namgrid.els[2][0];
+		this.inp5 = new azulInp({Field: 'Phone',Length: '250px', idx: 5});
+		thirdRow.appendChild(this.inp5.getInpEl());
+
+		this.inp1.setnextprev(this.inp2, this.inp5);
+		this.inp2.setnextprev(this.inp3, this.inp1);
+		this.inp3.setnextprev(this.inp4, this.inp2);
+		this.inp4.setnextprev(this.inp5, this.inp3);
+		this.inp5.setnextprev(this.inp1, this.inp4);
+
+		return namGridSec.inpgridDiv;
+	},
+
+	getInpValues: function() {
+		let inpVal = {
+			First: namesObj.inp1.getInpValue(),
+			Middle: namesObj.inp2.getInpValue(),
+			Last: namesObj.inp3.getInpValue(),
+			Email: namesObj.inp4.getInpValue(),
+			Phone: namesObj.inp5.getInpValue(),
+		};
+		const inpJsonStr = JSON.stringify(inpVal)
+		console.log('hello inp: ' + inpJsonStr);
+		return inpJsonStr;
+	},
+}
+
+let txtObj = {
+	name: 'txtForm',
+	render: function () {
+		let txtGridSec = new azulInpGrid({rows: 1, cols: 1, name: 'txtarea'});
+		let txtgrid = txtGridSec.inpgrid;
+
+		this.inp1 = new azulTxtInp({Field: 'Notes', Length: '250px', cols: 80, rows: 10, idx: 1});
+
+		let firstRow = txtgrid.els[0][0];
+		firstRow.appendChild(this.inp1.getInpEl());
+
+		return txtGridSec.inpgridDiv;
+	},
+
+	getInpValues: function() {
+		let inpVal = {
+			Txt: txtObj.inp1.getInpValue(),
+		};
+		const inpJsonStr = JSON.stringify(inpVal)
+		console.log('txt inp: ' + inpJsonStr);
+		return inpJsonStr;
+	},
+}
+
+let submitObj = {
+	butDivObj: {
+		style: {
+			height: '50px',
+			margin: '10px',
+			border: '1px dashed blue',
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+//			position: 'relative',
+		},
+		id: 'butDiv',
+		typ: 'div',
+	},
+	butStyl: {
+		height: '30px',
+		width: '100px',
+		border: '1px solid green',
+	},
+
+	butObj: {
+        style: this.butStyl,
+        typ: 'button',
+        textContent: 'submit',
+        evlist: {
+            click: namesObj.getInpValues,
+        },
+	},
+	render: function() {
+		let butDiv = azul.addElement(this.butDivObj);
+		const formBut = azul.addElement(this.butObj);
+		butDiv.appendChild(formBut);
+		return butDiv
+	},
+}
+
+let nameHdObj = {
+
+	hdNamesObj: {
+    	style: {
+        	color: 'Green',
+        	margin: 'auto',
+        	textAlign: 'center',
+        	padding: '0.5rem',
+        	fontSize: '2rem',
+    	},
+    	id: 'NameHd',
+    	textContent: 'Name Form',
+    	typ: 'h1',
+	},
+	render: function() {
+		return azul.addElement(this.hdNamesObj);
+	},
+}
+
+let headerSec = nameHdObj.render();
+azul.docbody.appendChild(headerSec);
+
+let namesSec = namesObj.render();
+azul.docbody.appendChild(namesSec);
+let txtSec = txtObj.render();
+azul.docbody.appendChild(txtSec);
+
+let submitSec = submitObj.render();
+azul.docbody.appendChild(submitSec);
+
